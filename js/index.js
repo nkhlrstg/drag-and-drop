d0cec66b7b294bbaa5ddcb16e97e002229ce86c3
nkhlrstg <117817856+nkhlrstg@users.noreply.github.com>
2022-11-09 15:29:20 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,408 @@
+
+function vibrate(duration = 200) {
+  if (navigator.vibrate) {
+    navigator.vibrate(duration);
+  }
+}
+
+const svgData = `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+viewBox="0 0 17.93 150.41" style="enable-background:new 0 0 17.93 150.41; position: absolute; top: 50%; right: 0; transform: translateY(-50%);height: 18vh;" xml:space="preserve">
+<style type="text/css">
+.st0{fill:#FFEFC6;}
+.st1{fill:#F7941D;}
+</style>
+<g>
+<path class="st0" d="M17.93,0v150.41H6.67c-3.69,0-6.67-2.99-6.67-6.67V6.67C0,2.99,2.99,0,6.67,0H17.93z"/>
+<g>
+ <path class="st1" d="M15.45,145.49l-6.06-1.15c-0.83-0.16-1.56-0.25-2.05-0.32v-0.77l0.95,0.13v-0.02
+   c-0.62-0.38-1.04-0.92-1.04-1.5c0-0.84,0.79-1.07,1.66-1.07c1.8,0,4.34,0.94,4.34,2.67c0,0.24-0.07,0.53-0.19,0.7v0.02l2.4,0.44
+   V145.49z M12.33,144.04c0.12-0.16,0.22-0.34,0.22-0.55c0-1.34-2.66-1.82-3.53-1.82c-0.56,0-1.01,0.11-1.01,0.56
+   c0,0.58,0.94,1.19,2.03,1.39L12.33,144.04z"/>
+ <path class="st1" d="M9.14,136.31c1.46,0,4.1,0.72,4.1,2.49c0,1.08-0.94,1.42-1.97,1.42c-1.56,0-4.03-0.84-4.03-2.57
+   C7.25,136.49,8.36,136.31,9.14,136.31z M11.19,139.35c0.66,0,1.34-0.05,1.34-0.66c0-1.06-2.47-1.5-3.45-1.5
+   c-0.6,0-1.12,0.11-1.12,0.61C7.97,138.76,10.06,139.35,11.19,139.35z"/>
+ <path class="st1" d="M7.35,134.58l3.26-0.04c0.62,0.01,1.02,0.01,1.46,0.05v-0.04c-0.43-0.13-0.88-0.29-1.42-0.52l-3.31-1.42
+   v-0.68l3.29-0.12c0.62-0.01,0.98-0.02,1.44,0.04v-0.05c-0.43-0.13-0.89-0.28-1.49-0.5l-3.24-1.28v-0.85l5.8,2.51v0.73l-3.14,0.16
+   c-0.46,0.02-0.94,0.01-1.45-0.04v0.04c0.53,0.17,1.03,0.32,1.45,0.5l3.14,1.37v0.76l-5.8,0.28V134.58z"/>
+ <path class="st1" d="M12.86,126.39c0.23,0.42,0.36,0.86,0.36,1.37c0,1.22-0.94,1.62-2.05,1.62c-1.57,0-3.92-0.88-3.92-2.53
+   c0-0.85,0.65-1.2,1.32-1.2c1.4,0,2,1.25,2.02,2.83c0.17,0.02,0.82,0.06,1.16-0.04c0.49-0.13,0.76-0.43,0.76-0.89
+   c0-0.46-0.16-0.77-0.31-1.03L12.86,126.39z M7.99,126.97c0,0.52,0.64,1.07,1.9,1.38c-0.01-0.92-0.32-1.9-1.26-1.9
+   C8.22,126.46,7.99,126.63,7.99,126.97z"/>
+ <path class="st1" d="M13.15,125.52l-3.9-0.73c-0.78-0.14-1.48-0.22-1.91-0.25v-0.76c0.31,0.01,0.62,0.04,1.09,0.1v-0.04
+   c-0.72-0.36-1.19-0.86-1.19-1.45c0-0.06,0.01-0.11,0.01-0.16l0.91,0.17c0,0.02-0.01,0.07-0.01,0.11c0,0.77,1.08,1.42,1.94,1.58
+   l3.05,0.58V125.52z"/>
+ <path class="st1" d="M12.86,119.61c0.23,0.42,0.36,0.86,0.36,1.37c0,1.22-0.94,1.62-2.05,1.62c-1.57,0-3.92-0.88-3.92-2.53
+   c0-0.85,0.65-1.2,1.32-1.2c1.4,0,2,1.25,2.02,2.83c0.17,0.02,0.82,0.06,1.16-0.04c0.49-0.13,0.76-0.43,0.76-0.89
+   s-0.16-0.77-0.31-1.03L12.86,119.61z M7.99,120.19c0,0.52,0.64,1.07,1.9,1.38c-0.01-0.92-0.32-1.9-1.26-1.9
+   C8.22,119.68,7.99,119.85,7.99,120.19z"/>
+ <path class="st1" d="M4.85,113.87l6.26,1.19c0.64,0.12,1.49,0.24,2.04,0.26v0.74l-0.91-0.07v0.02c0.6,0.29,1.01,0.83,1.01,1.39
+   c0,0.73-0.68,1.18-1.74,1.18c-1.98,0-4.26-1.04-4.26-2.75c0-0.23,0.1-0.47,0.22-0.61l-2.61-0.49V113.87z M8.22,115.4
+   c-0.16,0.11-0.26,0.29-0.26,0.54c0,1.12,2.19,1.76,3.38,1.76c0.68,0,1.16-0.16,1.16-0.64c0-0.42-0.53-0.98-1.68-1.21L8.22,115.4z"
+   />
+ <path class="st1" d="M4.85,109.97l3.24,0.62v-0.02c-0.6-0.42-0.84-0.83-0.84-1.31c0-0.9,0.94-1.14,1.64-1.14
+   c1.74,0,4.35,0.95,4.35,2.93c0,0.79-0.46,1.15-1.06,1.15c-0.24,0-0.49-0.04-0.65-0.07l-6.69-1.3V109.97z M11.4,111.25
+   c0.18,0.05,0.35,0.06,0.58,0.06c0.34,0,0.58-0.13,0.58-0.5c0-1.18-2.39-1.79-3.43-1.79c-0.67,0-1.1,0.16-1.1,0.62
+   c0,0.5,0.66,1.06,1.61,1.24L11.4,111.25z"/>
+ <path class="st1" d="M7.35,106.45l3.25-0.19c0.53-0.02,0.86-0.04,1.44-0.06v-0.02c-0.5-0.16-0.95-0.32-1.46-0.52l-3.23-1.2v-0.89
+   l4.71,1.94c1.3,0.53,2.37,1.14,3.03,1.91c0.29,0.35,0.5,0.68,0.58,0.9l-0.73,0.35c-0.12-0.25-0.3-0.56-0.55-0.86
+   c-0.3-0.36-0.83-0.76-1.26-0.94c-0.12-0.05-0.2-0.06-0.32-0.05l-5.46,0.52V106.45z"/>
+ <path class="st1" d="M12.62,103.63c-0.37,0-0.73-0.3-0.73-0.65c0-0.4,0.3-0.56,0.62-0.56c0.4,0,0.73,0.29,0.73,0.65
+   C13.25,103.43,12.98,103.63,12.62,103.63z M8.21,102.79c-0.36,0-0.72-0.3-0.72-0.64c0-0.41,0.29-0.58,0.61-0.58
+   c0.4,0,0.73,0.29,0.73,0.65S8.57,102.79,8.21,102.79z"/>
+</g>
+<g>
+ <path class="st1" d="M13.43,89.59c0.22,0.43,0.45,1.4,0.45,2.66c0,3.58-2.23,5.42-5.18,5.42c-3.53,0-5.5-2.52-5.5-5.65
+   c0-1.21,0.25-2.14,0.46-2.55l1.86,0.48c-0.2,0.48-0.38,1.14-0.38,1.97c0,1.86,1.12,3.3,3.43,3.3c2.07,0,3.38-1.23,3.38-3.32
+   c0-0.71-0.15-1.49-0.34-1.95L13.43,89.59z"/>
+ <path class="st1" d="M9.88,81c2.75,0,4.01,1.95,4.01,3.96c0,2.2-1.44,3.89-3.87,3.89s-3.98-1.6-3.98-4.01
+   C6.04,82.54,7.62,81,9.88,81z M9.95,86.44c1.29,0,2.26-0.54,2.26-1.54c0-0.91-0.91-1.49-2.26-1.49c-1.12,0-2.26,0.43-2.26,1.49
+   C7.7,86.03,8.85,86.44,9.95,86.44z"/>
+ <path class="st1" d="M8.6,79.56c-0.94,0-1.74,0.03-2.4,0.06V77.6l1.03-0.11v-0.05c-0.48-0.31-1.2-1.08-1.2-2.32
+   c0-1.54,1.01-2.69,3.23-2.69h4.46v2.34H9.55c-0.97,0-1.63,0.34-1.63,1.18c0,0.65,0.45,1.03,0.88,1.18
+   c0.14,0.06,0.37,0.09,0.58,0.09h4.33v2.34H8.6z"/>
+ <path class="st1" d="M4.05,68.04h2.15v-1.67h1.72v1.67h2.72c0.91,0,1.32-0.22,1.32-0.92c0-0.29-0.03-0.52-0.06-0.69l1.77-0.02
+   c0.12,0.31,0.2,0.86,0.2,1.52c0,0.75-0.26,1.38-0.65,1.75c-0.45,0.43-1.17,0.65-2.23,0.65H7.93v1H6.21v-1H4.58L4.05,68.04z"/>
+ <path class="st1" d="M10.71,63.3c0.97-0.08,1.43-1.03,1.43-2.12c0-0.8-0.11-1.44-0.31-2.07l1.58-0.31
+   c0.31,0.77,0.46,1.71,0.46,2.72c0,2.55-1.48,4.01-3.83,4.01c-1.91,0-4.01-1.18-4.01-3.8c0-2.43,1.89-3.35,3.75-3.35
+   c0.4,0,0.75,0.05,0.92,0.08V63.3z M9.11,60.55c-0.57,0-1.52,0.25-1.52,1.32c0,0.98,0.89,1.38,1.52,1.44V60.55z"/>
+ <path class="st1" d="M8.6,56.99c-0.94,0-1.74,0.03-2.4,0.06v-2.03l1.03-0.11v-0.05c-0.48-0.31-1.2-1.08-1.2-2.32
+   c0-1.54,1.01-2.69,3.23-2.69h4.46v2.34H9.55c-0.97,0-1.63,0.34-1.63,1.18c0,0.65,0.45,1.03,0.88,1.18
+   c0.14,0.06,0.37,0.09,0.58,0.09h4.33v2.34H8.6z"/>
+ <path class="st1" d="M4.12,45.57c0.65,0,1.17,0.49,1.17,1.26c0,0.74-0.52,1.23-1.17,1.21c-0.68,0.02-1.18-0.48-1.18-1.23
+   S3.44,45.59,4.12,45.57z M13.72,47.99H6.21v-2.34h7.51V47.99z"/>
+ <path class="st1" d="M9.37,41.44v-0.03c-0.31-0.17-0.61-0.35-0.91-0.54l-2.26-1.52v-2.81l3.04,2.69l4.47-3.07v2.87l-3.09,1.81
+   l0.74,0.6h2.35v2.34H2.81v-2.34H9.37z"/>
+ <path class="st1" d="M12.47,35.56c-0.83,0-1.43-0.57-1.43-1.38c0-0.81,0.58-1.37,1.43-1.37c0.81,0,1.41,0.55,1.41,1.38
+   C13.89,34.99,13.29,35.56,12.47,35.56z"/>
+ <path class="st1" d="M13.53,25.76c0.18,0.42,0.34,1.2,0.34,2.09c0,2.43-1.48,3.98-3.84,3.98c-2.2,0-3.99-1.51-3.99-4.3
+   c0-0.61,0.11-1.29,0.29-1.78l1.74,0.37c-0.12,0.28-0.23,0.69-0.23,1.31c0,1.23,0.88,2.03,2.11,2.01c1.38,0,2.1-0.92,2.1-2.06
+   c0-0.55-0.09-0.98-0.23-1.34L13.53,25.76z"/>
+ <path class="st1" d="M9.88,17.22c2.75,0,4.01,1.95,4.01,3.96c0,2.2-1.44,3.89-3.87,3.89s-3.98-1.6-3.98-4.01
+   C6.04,18.76,7.62,17.22,9.88,17.22z M9.95,22.66c1.29,0,2.26-0.54,2.26-1.54c0-0.91-0.91-1.49-2.26-1.49
+   c-1.12,0-2.26,0.43-2.26,1.49C7.7,22.24,8.85,22.66,9.95,22.66z"/>
+ <path class="st1" d="M8.6,15.78c-0.94,0-1.74,0.03-2.4,0.06v-1.97l1.01-0.11v-0.05c-0.48-0.32-1.18-0.98-1.18-2.27
+   c0-0.97,0.49-1.74,1.28-2.06V9.36C6.93,9.08,6.62,8.74,6.4,8.39C6.16,7.97,6.04,7.51,6.04,6.96c0-1.44,1.01-2.54,3.26-2.54h4.43
+   V6.7H9.63c-1.09,0-1.72,0.35-1.72,1.11c0,0.54,0.37,0.92,0.81,1.08c0.17,0.06,0.41,0.09,0.6,0.09h4.39v2.27H9.51
+   c-0.95,0-1.6,0.34-1.6,1.08c0,0.6,0.46,0.95,0.85,1.09c0.18,0.08,0.4,0.09,0.58,0.09h4.38v2.27H8.6z"/>
+</g>
+</g>
+</svg>`
+let svgDiv = document.createElement("div");
+svgDiv.innerHTML = svgData;
+document.body.appendChild(svgDiv)
+
+
+let val;
+let counter = 1;
+const h2 = document.querySelector("h2");
+const topDiv = document.querySelector("#top");
+const imgDiv = document.querySelector("#main-image");
+const bottomDiv = document.querySelector("#bottom");
+const dropBoxWrapper = document.querySelector(".dropBoxWrapper");
+const dropBox = document.querySelector(".dropBox");
+const question = document.querySelector("#question");
+const main_img = document.querySelector("#main-image");
+const option = document.querySelectorAll(".option");
+const opt1 = document.querySelector("#opt1");
+const opt2 = document.querySelector("#opt2");
+const rightImg = document.querySelector("#right");
+const wrongImg = document.querySelector("#wrong");
+const startBtn = document.querySelector("#start");
+const resetBtn = document.querySelector("#resetBtn");
+const submitBtn = document.querySelector(".submit");
+
+const qHeadAudio = document.querySelector("#qHeadAudio");
+const wrongAudio = document.querySelector("#wrongAudio");
+const rightAudio = document.querySelector("#rightAudio");
+const tryAgainAudio = document.querySelector("#tryagainAudio");
+
+document.querySelector('html').style.height = document.documentElement.clientHeight + 'px';
+topDiv.style.display = 'none';
+imgDiv.style.display = 'none';
+bottomDiv.style.display = 'none';
+svgDiv.style.display = "none";
+startBtn.addEventListener('click', ()=>{
+  qHeadAudio.play();
+  startBtn.style.display = 'none';
+  topDiv.style.display = 'block';
+  imgDiv.style.display = 'block';
+  bottomDiv.style.display = 'flex';
+  svgDiv.style.display = "block";
+})
+
+
+fetch('data.json')
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+  h2.textContent = data[0].heading_text;
+  h2.style.color = data[0].heading_txtColor;
+  h2.style.backgroundColor = data[0].heading_bgColor;
+  question.textContent = data[counter].question;
+  main_img.style.backgroundImage = "url('" + data[counter].question_image + "')";
+  question.setAttribute('data-before', counter + '.');
+  opt1.textContent = data[counter].options[0];
+  opt2.textContent = data[counter].options[1];
+
+  option.forEach((opt) => {
+    if(opt.textContent == data[counter].correct_option){
+      dropBoxWrapper.classList.add(data[counter].correct_option);
+      dropBox.classList.add(data[counter].correct_option);
+      opt.classList.add('correct');
+    }
+  });
+  const defaultState = {
+    defVal: "",
+    defWidth: "100%",
+    defColor: "#000",
+    defBgColor: "#ffffc6",
+    defBorder: "2px dotted #c98020",
+    defDisplay: "inline",
+    defPosition: "",
+    defLeft: "0",
+  };
+
+  const details = navigator.userAgent;
+  const regexp = /android|iphone|kindle|ipad/i;
+  const isMobileDevice = regexp.test(details);
+
+  submitBtn.addEventListener("click", () => {
+    vibrate();
+    resetBtn.style.visibility = "hidden";
+
+    if(counter < (data.length - 1)){
+      counter++;
+      if (isMobileDevice) {
+        dropBoxWrapper.classList.remove("done");
+        dropBox.style.width = defaultState.defWidth;
+        dropBoxWrapper.style.color = defaultState.defColor;
+        dropBoxWrapper.style.background = defaultState.defBgColor;
+        dropBoxWrapper.style.border = defaultState.defBorder;
+        rightImg.style.transform = "scale(0)";
+        wrongImg.style.transform = "scale(0)";
+        dropBox.style.color = defaultState.defColor;
+        dropBox.textContent = "";
+        question.textContent = data[counter].question;
+        dropBoxWrapper.classList.remove(dropBoxWrapper.classList[1]);
+        dropBox.classList.remove(dropBox.classList[1]);
+        dropBoxWrapper.classList.add(data[counter].correct_option);
+        dropBox.classList.add(data[counter].correct_option);
+        if(data[counter].question_image != ""){
+          main_img.style.backgroundImage = "url('" + data[counter].question_image + "')";
+        }
+        question.setAttribute("data-before", counter + ".");
+        opt1.textContent = data[counter].options[0];
+        opt2.textContent = data[counter].options[1];
+        option.forEach((opt) => {
+          opt.style.position = "unset";
+          opt.setAttribute("draggable", "true");
+        });
+        val = defaultState.defVal;
+      }
+      else
+      {
+        question.textContent = data[counter].question;
+        dropBoxWrapper.classList.remove(dropBoxWrapper.classList[1]);
+        dropBox.classList.remove(dropBox.classList[1]);
+        dropBoxWrapper.classList.add(data[counter].correct_option);
+        dropBox.classList.add(data[counter].correct_option);
+        question.setAttribute("data-before", counter + ".");
+          option.forEach((opt) => {
+          opt.style.display = defaultState.defDisplay;
+          opt.setAttribute("draggable", "true");
+        });
+
+        val = defaultState.defVal;
+        dropBox.innerText = defaultState.defVal;
+        dropBox.style.width = defaultState.defWidth;
+        dropBox.style.color = defaultState.defColor;
+        dropBox.classList.remove("done");
+        dropBoxWrapper.style.background = defaultState.defBgColor;
+        dropBoxWrapper.style.border = defaultState.defBorder;
+        if(data[counter].question_image != ""){
+          main_img.style.backgroundImage = "url('" + data[counter].question_image + "')";
+        }
+        opt1.textContent = data[counter].options[0];
+        opt2.textContent = data[counter].options[1];
+        rightImg.style.transform = "scale(0)";
+        wrongImg.style.transform = "scale(0)";
+      }
+    }
+    else{
+      document.write(`<img style="text-align:center; width: clamp(200px, 100%, 600px);" src=${data[0].finish_image}></img>`);
+      document.body.style.margin = "0";
+      document.body.style.padding = "0";
+      document.body.style.display = "flex";
+      document.body.style.alignItems = "start";
+      document.body.style.justifyContent = "center";
+  }
+  });
+
+  resetBtn.addEventListener("click", ()=> {
+    vibrate();
+    resetBtn.style.visibility = "hidden";
+    if (isMobileDevice) {
+      dropBoxWrapper.classList.remove("done");
+      dropBoxWrapper.style.color = defaultState.defColor;
+      dropBoxWrapper.style.background = defaultState.defBgColor;
+      dropBoxWrapper.style.border = defaultState.defBorder;
+      dropBox.style.width = defaultState.defWidth;
+      dropBox.style.color = defaultState.defColor;
+      option.forEach((opt) => {
+        opt.style.position = "unset";
+        opt.setAttribute("draggable", "true");
+      });
+      wrongImg.style.transform = "scale(0)";
+      dropBox.textContent = "";
+      val = defaultState.defVal;
+    }
+    else{
+      dropBoxWrapper.style.background = defaultState.defBgColor;
+      dropBoxWrapper.style.border = defaultState.defBorder;
+      dropBox.style.color = defaultState.defBgColor;
+      dropBox.style.width = defaultState.defWidth;
+      dropBox.textContent = defaultState.defVal;
+      wrongImg.style.transform = "scale(0)";
+      
+      option.forEach((opt) => {
+        opt.style.position = "unset";
+        opt.setAttribute("draggable", "true");
+      });
+    }
+  })
+
+
+  if (isMobileDevice)
+  {
+    document.body.addEventListener("touchstart", touchStarting);
+    document.body.addEventListener("touchmove", touchMoving);
+    document.body.addEventListener("touchend", touchEnding);
+  }
+  else
+  {
+    question.style.display = "block";
+    option.forEach((opt) => {
+      opt.addEventListener("dragstart", dragStarting);
+    });
+    dropBox.addEventListener("dragover", draggingOver);
+    dropBox.addEventListener("drop", dropping);
+  }
+})
+
+function touchStarting(evt) {
+    el = evt.target;
+    if (el.getAttribute("draggable") == "true")
+    {
+      avail = "available";
+    }
+    else
+    {
+      avail = "";
+    }
+    val = el.innerText;
+}
+  
+function touchMoving(evt) {
+    if (avail == "available") {
+      el.style.position = "absolute";
+      el.style.left = evt.touches[0].pageX - 50 + "px";
+      el.style.top = evt.touches[0].pageY - 25 + "px";
+    }
+}
+  
+function touchEnding(ev) {
+    vibrate(100);
+  
+    if (avail == "available")
+    {
+      option.forEach((opt) =>
+      {
+        opt.removeAttribute("draggable");
+      });
+  
+          if (!dropBoxWrapper.classList.contains("done"))
+            {
+              if (dropBoxWrapper.classList.contains(val))
+                  {
+                    rightAudio.play();
+                    dropBox.innerText = val;
+                    dropBoxWrapper.classList.add("done");
+                    dropBox.style.width = "70%";
+                    dropBoxWrapper.style.color = "green";
+                    dropBoxWrapper.style.background = "#ccffcc";
+                    dropBoxWrapper.style.border = "2px dotted green";
+                    option.forEach((opt) => {
+                      opt.style.position = "unset";
+                    });
+  
+                    setTimeout(() => {
+                      rightImg.style.transform = "scale(1.5)";
+                    }, 111);
+                  }
+              else
+                  {
+                    wrongAudio.play();
+                    dropBox.innerText = val;
+                    dropBoxWrapper.classList.add("done");
+                    dropBoxWrapper.style.color = "red";
+                    dropBox.style.width = "70%";
+                    dropBoxWrapper.style.background = "#ffe6e6";
+                    dropBoxWrapper.style.border = "2px dotted red";
+                    option.forEach((opt) => {opt.style.position = "unset";});
+                    setTimeout(() => {wrongImg.style.transform = "scale(1.5)";}, 111);
+                    wrongAudio.addEventListener('ended', ()=>{
+                      resetBtn.style.visibility = "visible";
+                      tryAgainAudio.play();
+                    })
+                  }
+            }
+    }
+}
+ 
+function dragStarting(dragStartEv) {
+  val = dragStartEv.target.textContent;
+}
+    
+function draggingOver(dragEv) {
+  dragEv.preventDefault();
+}
+   
+function dropping() {
+  option.forEach((opt) => {
+    opt.removeAttribute("draggable");
+  });
+  if (!dropBox.classList.contains("done"))
+  {
+    if (dropBox.classList.contains(val))
+    {
+      rightAudio.play();
+      dropBox.innerText = val;
+      dropBox.style.width = "75%";
+      dropBox.style.color = "green";
+      dropBoxWrapper.style.background = "#ccffcc";
+      dropBoxWrapper.style.border = "2px dotted green";
+      setTimeout(() => {
+        rightImg.style.transform = "scale(1.5)";
+      }, 111);
+    
+      dropBox.classList.add("done");
+    }
+    else
+    {
+      wrongAudio.play();
+      dropBox.innerText = val;
+      dropBox.style.color = "red";
+      dropBox.style.width = "75%";
+      dropBoxWrapper.style.background = "#ffe6e6";
+      dropBoxWrapper.style.border = "2px dotted red";
+      setTimeout(() => {
+        wrongImg.style.transform = "scale(1.5)";
+      }, 111);
+      wrongAudio.addEventListener('ended', ()=>{
+        resetBtn.style.visibility = "visible";
+        tryAgainAudio.play();
+      })
+    }
+  }
+}
